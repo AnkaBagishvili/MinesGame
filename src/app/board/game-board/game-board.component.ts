2ff77ee665952bdb1d40c4990e8c863a8f418137
AnkaBagishvili <lukiana.3@gmail.com>
2025-02-10 23:54:23 +0400

    implemented randomizer component

--- src/app/board/game-board/game-board.component.ts
+++ src/app/board/game-board/game-board.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PlayGroundComponent } from '../play-ground/play-ground.component';
 import { ProgressComponent } from '../../layouts/progress/progress.component';
+import { RandomizerComponent } from '../../layouts/randomizer/randomizer.component';
 
 @Component({
   selector: 'app-game-board',
   standalone: true,
-  imports: [PlayGroundComponent, ProgressComponent],
+  imports: [PlayGroundComponent, ProgressComponent, RandomizerComponent],
   templateUrl: './game-board.component.html',
   styleUrl: './game-board.component.scss',
 })
